1a14a073a9e779e135139373caed4340fff3baa2
William Kaufmann <34726013+wkaufmann04@users.noreply.github.com>
2026-09-21 20:36:03 -0400

    Fix duplicate API request in streamlit_app.py
    Removed duplicate API request for watermelon data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,5 @@ import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon")  
 #st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
-
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.write(smoothiefroot_response.status_code)
 st.write(smoothiefroot_response.text)
